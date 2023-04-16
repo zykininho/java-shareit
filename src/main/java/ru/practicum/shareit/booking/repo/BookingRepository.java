10637a4430896668954a228c38f2a7a0fa7ca646
MZykin <zykininho@gmail.com>
2023-04-16 16:09:10 +0600

    feat: added method for search bookings

--- src/main/java/ru/practicum/shareit/booking/repo/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repo/BookingRepository.java
@@ -1,10 +1,14 @@
 package ru.practicum.shareit.booking.repo;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.booking.model.Booking;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-
+    List<Booking> findByBooker_IdAndEndIsBefore(Long bookerId, LocalDateTime end, Sort sort);
 
 }
