caf71a3c8ef527799c128b25140a8bb221180319
MZykin <zykininho@gmail.com>
2023-05-01 23:35:05 +0600

    feat: added methods for ItemRequestController, ItemRequestService

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -1,5 +1,17 @@
 package ru.practicum.shareit.request.service;
 
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import java.util.List;
+
 public interface ItemRequestService {
+
+    ItemRequestDto create(long userId, ItemRequestDto itemRequestDto);
+
+    List<ItemRequestDto> getAll(long userId);
+
+    ItemRequestDto getItemRequest(long userId, long requestId);
+
+    List<ItemRequestDto> search(long userId, String from, String size);
 
 }
--- src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -3,10 +3,33 @@ package ru.practicum.shareit.request.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ItemRequestServiceImpl implements ItemRequestService {
+
+    @Override
+    public ItemRequestDto create(long userId, ItemRequestDto itemRequestDto) {
+        return null;
+    }
+
+    @Override
+    public List<ItemRequestDto> getAll(long userId) {
+        return null;
+    }
+
+    @Override
+    public ItemRequestDto getItemRequest(long userId, long requestId) {
+        return null;
+    }
+
+    @Override
+    public List<ItemRequestDto> search(long userId, String from, String size) {
+        return null;
+    }
 
 }
