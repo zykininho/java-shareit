f47fc3f354ea7aae79767f5989e931ae8459e027
MZykin <zykininho@gmail.com>
2023-05-08 19:58:28 +0600

    fix: удалены неиспользуемые импорты

--- server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.mapper.BookingMapper;
