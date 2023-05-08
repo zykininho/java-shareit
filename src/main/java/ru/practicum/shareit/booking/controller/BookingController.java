1dba4c4047f3f83ea46232715496829e9486811e
MZykin <zykininho@gmail.com>
2023-05-08 19:45:28 +0600

    fix: рефакторинг

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -18,29 +18,29 @@ public class BookingController {
 
     private final BookingService bookingService;
 
-    /*@PostMapping
+    @PostMapping
     public ResponseEntity<BookingDto> add(@RequestHeader(value = "X-Sharer-User-Id") long userId,
                                        @RequestBody BookingFromUserDto bookingFromUser) {
         log.info("Received POST-request at /bookings endpoint from user id={}", userId);
         return ResponseEntity.ok().body(bookingService.addNewBooking(userId, bookingFromUser));
-    }*/
+    }
 
-    /*@PatchMapping("/{bookingId}")
+    @PatchMapping("/{bookingId}")
     public ResponseEntity<BookingDto> update(@RequestHeader(value = "X-Sharer-User-Id") long userId,
                                           @PathVariable long bookingId,
                                           @RequestParam String approved) {
         log.info("Received PATCH-request at /bookings/{} endpoint from user id={}", bookingId, userId);
         return ResponseEntity.ok().body(bookingService.updateBooking(userId, bookingId, approved));
-    }*/
+    }
 
-    /*@GetMapping("/{bookingId}")
+    @GetMapping("/{bookingId}")
     public ResponseEntity<BookingDto> get(@RequestHeader(value = "X-Sharer-User-Id") long userId,
                                              @PathVariable long bookingId) {
         log.info("Received GET-request at /bookings/{} endpoint from user id={}", userId, bookingId);
         return ResponseEntity.ok().body(bookingService.getBooking(userId, bookingId));
-    }*/
+    }
 
-    /*@GetMapping
+    @GetMapping
     public ResponseEntity<List<BookingDto>> getUserBookings(@RequestHeader(value = "X-Sharer-User-Id") long userId,
                                                             @RequestParam(defaultValue = "ALL") String state,
                                                             @RequestParam(required = false) Integer from,
@@ -48,9 +48,9 @@ public class BookingController {
         log.info("Received GET-request at /bookings?state={}&from={}&size={} endpoint from user id={}",
                 state, from, size, userId);
         return ResponseEntity.ok().body(bookingService.getUserBookings(userId, state, from, size));
-    }*/
+    }
 
-    /*@GetMapping("/owner")
+    @GetMapping("/owner")
     public ResponseEntity<List<BookingDto>> getItemsOwnerBookings(@RequestHeader(value = "X-Sharer-User-Id") long userId,
                                                                   @RequestParam(defaultValue = "ALL") String state,
                                                                   @RequestParam(required = false) Integer from,
@@ -58,6 +58,6 @@ public class BookingController {
         log.info("Received GET-request at /bookings/owner?state={}&from={}&size={} endpoint from user id={}",
                 state, from, size, userId);
         return ResponseEntity.ok().body(bookingService.getItemsOwnerBookings(userId, state, from, size));
-    }*/
+    }
 
 }
